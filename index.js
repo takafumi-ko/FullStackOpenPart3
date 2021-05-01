cbd32246b538486ee6d01c8e1e60a00523e0f762
古賀隆文 <t-koga@unext.jp>
2021-05-01 20:36:28 +0900

    Exercises 3.9

--- index.js
+++ index.js
@@ -94,7 +94,8 @@ app.delete('/api/persons/:id', (request, response) => {
     }
 })
 
-const PORT = 3001
+
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
